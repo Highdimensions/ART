8d810545d57fb32558018d5800306f7909cc457b
Vladimir Marko <vmarko@google.com>
2019-05-13 11:37:22 +0100

    StringBuilder append pattern with char[]/StringBuilder.
    TODO: Determine the best course of action given the
    exceptions that can be thrown.
    
    Bug: 19575890
    Test: 697-checker-string-append
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: aosp_taimen-userdebug boots.
    Test: run-gtests.sh
    Test: testrunner.py --target --optimizing
    Change-Id: I7615e98ec52283d71b33c4cc8c3ea16c73d4eb66

--- compiler/optimizing/instruction_simplifier.cc
+++ compiler/optimizing/instruction_simplifier.cc
@@ -3078,9 +3078,8 @@ static bool TryReplaceStringBuilderAppend(CodeGenerator* codegen, HInvoke* invok
           arg = StringBuilderAppend::Argument::kString;
           break;
         case Intrinsics::kStringBuilderAppendCharArray:
-          // TODO: Unimplemented, StringBuilder.append(char[]) can throw NPE and we would
-          // not have the correct stack trace for it.
-          return false;
+          arg = StringBuilderAppend::Argument::kCharArray;
+          break;
         case Intrinsics::kStringBuilderAppendBoolean:
           arg = StringBuilderAppend::Argument::kBoolean;
           break;
@@ -3111,11 +3110,9 @@ static bool TryReplaceStringBuilderAppend(CodeGenerator* codegen, HInvoke* invok
           DCHECK(input_type != nullptr);
           if (input_type.Get() == GetClassRoot<mirror::String>()) {
             arg = StringBuilderAppend::Argument::kString;
+          } else if (input_type->DescriptorEquals("Ljava/lang/StringBuilder;")) {
+            arg = StringBuilderAppend::Argument::kStringBuilder;
           } else {
-            // TODO: Check and implement for StringBuilder. We could find the StringBuilder's
-            // internal char[] inconsistent with the length, or the string compression
-            // of the result could be compromised with a concurrent modification, and
-            // we would need to throw appropriate exceptions.
             return false;
           }
           break;
