bda4b883698b9af05506e57a0021c730f8a7a25a
Chris Jones <christopher.jones@arm.com>
2024-01-05 16:30:59 +0000

    [Sim] Setup custom simulator
    Setup a custom ART simulator that can run Arm64 tests on an x86_64
    host machine. This patch does not support the actual running of the
    simulator but sets up the necessary components to enable this in
    future patches. Namely this patch does the following:
    
    - Refactor CodeSimulator and CodeSimulatorContainer classes to
      separate the semantics of basic and runtime-aware simulation.
    - Intercept branches to C++ entrypoints for the custom simulator.
      C++ entrypoint functions have a native context (are compiled as
      x86_64) and therefore there needs to be a transition in ISA/ABI
      when calling these functions from a quick context, i.e:
      entrypoints written in Arm64 assembly. To perform this transition,
      VIXL "runtime calls" will be used via the branch interception
      interface provided by VIXL.
    - Add a simulator version of the generic JNI trampoline to be used
      to handle the transition between native and quick code.
    
    Authors: Artem Serov <artem.serov@linaro.org>
             Greg Cawthorne <greg.cawthorne@linaro.org>
             Chris Jones <christopher.jones@arm.com>
    
    Test: test.py --host --target
    Change-Id: I7920cd376f9822fb866ecc6337b6cf4ff69c4cc2

--- simulator/code_simulator_arm64.cc
+++ simulator/code_simulator_arm64.cc
@@ -16,65 +16,182 @@
 
 #include "code_simulator_arm64.h"
 
-#include <android-base/logging.h>
+#include "arch/arm64/asm_support_arm64.h"
+#include "arch/instruction_set.h"
+#include "base/memory_region.h"
+#include "entrypoints/quick/runtime_entrypoints_list.h"
+
+#include "code_simulator_container.h"
 
 using namespace vixl::aarch64;  // NOLINT(build/namespaces)
 
 namespace art {
+
+extern "C" const void* GetQuickInvokeStub();
+extern "C" const void* GetQuickInvokeStaticStub();
+
 namespace arm64 {
 
-// VIXL has not been tested on 32bit architectures, so Simulator is not always
-// available. To avoid linker error on these architectures, we check if we can simulate
-// in the beginning of following methods, with compile time constant `kCanSimulate`.
-// TODO: when Simulator is always available, remove the these checks.
+BasicCodeSimulatorArm64* BasicCodeSimulatorArm64::CreateBasicCodeSimulatorArm64(size_t stack_size) {
+  BasicCodeSimulatorArm64* simulator = new BasicCodeSimulatorArm64();
+  simulator->InitInstructionSimulator(stack_size);
+  return simulator;
+}
 
-CodeSimulatorArm64* CodeSimulatorArm64::CreateCodeSimulatorArm64() {
-  if (kCanSimulate) {
-    return new CodeSimulatorArm64();
-  } else {
-    return nullptr;
+BasicCodeSimulatorArm64::BasicCodeSimulatorArm64()
+    : BasicCodeSimulator(), decoder_(nullptr), instruction_simulator_(nullptr) {
+  CHECK(CanSimulate(InstructionSet::kArm64));
+  decoder_.reset(new Decoder());
+}
+
+Simulator* BasicCodeSimulatorArm64::CreateNewInstructionSimulator(SimStack::Allocated&& stack) {
+  return new Simulator(decoder_.get(), stdout, std::move(stack));
+}
+
+static constexpr size_t kSimulatorLimitGuardSize = 2 * kPageSize;
+
+void BasicCodeSimulatorArm64::InitInstructionSimulator(size_t stack_size) {
+  SimStack stack_builder;
+  stack_builder.SetLimitGuardSize(kSimulatorLimitGuardSize);
+  stack_builder.SetUsableSize(stack_size);
+  SimStack::Allocated stack = stack_builder.Allocate();
+
+  instruction_simulator_.reset(CreateNewInstructionSimulator(std::move(stack)));
+  instruction_simulator_->SetVectorLengthInBits(kArm64DefaultSVEVectorLength);
+  if (VLOG_IS_ON(simulator)) {
+    instruction_simulator_->SetColouredTrace(true);
+    instruction_simulator_->SetTraceParameters(LOG_DISASM | LOG_WRITE | LOG_REGS);
   }
 }
 
-CodeSimulatorArm64::CodeSimulatorArm64()
-    : CodeSimulator(), decoder_(nullptr), simulator_(nullptr) {
-  DCHECK(kCanSimulate);
-  decoder_ = new Decoder();
-
-  SimStack stack_builder;
-  stack_builder.SetLimitGuardSize(0x4000);
-  stack_builder.SetUsableSize(0x4000);
-  SimStack::Allocated stack = stack_builder.Allocate();
-
-  simulator_ = new Simulator(decoder_, stdout, std::move(stack));
-  simulator_->SetVectorLengthInBits(kArm64DefaultSVEVectorLength);
+void BasicCodeSimulatorArm64::RunFrom(intptr_t code_buffer) {
+  instruction_simulator_->RunFrom(reinterpret_cast<const vixl::aarch64::Instruction*>(code_buffer));
 }
 
-CodeSimulatorArm64::~CodeSimulatorArm64() {
-  DCHECK(kCanSimulate);
-  delete simulator_;
-  delete decoder_;
+bool BasicCodeSimulatorArm64::GetCReturnBool() const {
+  return instruction_simulator_->ReadWRegister(0);
 }
 
-void CodeSimulatorArm64::RunFrom(intptr_t code_buffer) {
-  DCHECK(kCanSimulate);
-  simulator_->RunFrom(reinterpret_cast<const Instruction*>(code_buffer));
+int32_t BasicCodeSimulatorArm64::GetCReturnInt32() const {
+  return instruction_simulator_->ReadWRegister(0);
 }
 
-bool CodeSimulatorArm64::GetCReturnBool() const {
-  DCHECK(kCanSimulate);
-  return simulator_->ReadWRegister(0);
+int64_t BasicCodeSimulatorArm64::GetCReturnInt64() const {
+  return instruction_simulator_->ReadXRegister(0);
 }
 
-int32_t CodeSimulatorArm64::GetCReturnInt32() const {
-  DCHECK(kCanSimulate);
-  return simulator_->ReadWRegister(0);
+#ifdef ART_USE_SIMULATOR
+
+//
+// Special registers defined in asm_support_arm64.s.
+//
+
+// Frame Pointer.
+static const unsigned kFp = 29;
+// Stack Pointer.
+static const unsigned kSp = 31;
+
+class ARTSimulator final : public Simulator {
+ public:
+  ARTSimulator(Decoder* decoder, FILE* stream, SimStack::Allocated stack)
+      : Simulator(decoder, stream, std::move(stack)) {
+    // Setup C++ entrypoint functions to be intercepted. This list should include C++ entrypoint
+    // functions from runtime_entrypoints_list.h which are called from quick assembly code. These
+    // functions are part of the runtime and so branch interceptions are needed to perform an ISA
+    // transition from the quick code ISA to the runtime ISA.
+    RegisterBranchInterception(artQuickResolutionTrampoline);
+    RegisterBranchInterception(artQuickToInterpreterBridge);
+    RegisterBranchInterception(artQuickGenericJniTrampoline);
+    RegisterBranchInterception(artThrowDivZeroFromCode);
+    RegisterBranchInterception(artDeliverPendingExceptionFromCode);
+    RegisterBranchInterception(artContextCopyForLongJump);
+    RegisterBranchInterception(artQuickProxyInvokeHandler);
+    RegisterBranchInterception(artInvokeObsoleteMethod);
+    RegisterBranchInterception(artMethodExitHook);
+
+    RegisterBranchInterception(artArm64SimulatorGenericJNIPlaceholder,
+                               [this]([[maybe_unused]] uint64_t addr)
+                               REQUIRES_SHARED(Locks::mutator_lock_) {
+      uint64_t native_code_ptr = static_cast<uint64_t>(ReadXRegister(0));
+      ArtMethod** simulated_reserved_area = reinterpret_cast<ArtMethod**>(ReadXRegister(1));
+      Thread* self = reinterpret_cast<Thread*>(ReadXRegister(2));
+
+      uint64_t fp_result = 0.0;
+      int64_t gpr_result = artQuickGenericJniTrampolineSimulator(
+          native_code_ptr,
+          reinterpret_cast<void*>(simulated_reserved_area),
+          reinterpret_cast<void*>(&fp_result));
+
+      jvalue jval;
+      jval.j = gpr_result;
+      uint64_t result_end = artQuickGenericJniEndTrampoline(self, jval, fp_result);
+
+      WriteXRegister(0, result_end);
+      WriteDRegister(0, bit_cast<double>(result_end));
+    });
+  }
+
+  virtual ~ARTSimulator() {}
+
+  uint8_t* GetStackBase() {
+    return reinterpret_cast<uint8_t*>(memory_.GetStack().GetBase());
+  }
+
+
+  // TODO(Simulator): Maybe integrate these into vixl?
+  int64_t get_sp() const {
+    return ReadRegister<int64_t>(kSp, Reg31IsStackPointer);
+  }
+
+  int64_t get_x(int32_t n) const {
+    return ReadRegister<int64_t>(n, Reg31IsStackPointer);
+  }
+
+  int64_t get_lr() const {
+    return ReadRegister<int64_t>(kLinkRegCode);
+  }
+
+  int64_t get_fp() const {
+    return ReadXRegister(kFp);
+  }
+
+  // Register a branch interception to a function which returns TwoWordReturn. VIXL does not
+  // currently support returning composite types from runtime calls so this is a specialised case.
+  template <typename... P>
+  void RegisterTwoWordReturnInterception(TwoWordReturn (*func)(P...)) {
+    RegisterBranchInterception(reinterpret_cast<void (*)()>(func),
+                               [this, func]([[maybe_unused]] uint64_t addr) {
+      ABI abi;
+      std::tuple<P...> arguments{
+          ReadGenericOperand<P>(abi.GetNextParameterGenericOperand<P>())...};
+
+      TwoWordReturn res = DoRuntimeCall(func, arguments, __local_index_sequence_for<P...>{});
+
+      // Method pointer.
+      WriteXRegister(0, res.lo);
+      // Code pointer.
+      WriteXRegister(1, res.hi);
+    });
+  }
+};
+
+CodeSimulatorArm64* CodeSimulatorArm64::CreateCodeSimulatorArm64(size_t stack_size) {
+    CodeSimulatorArm64* simulator = new CodeSimulatorArm64();
+    simulator->InitInstructionSimulator(stack_size);
+    return simulator;
 }
 
-int64_t CodeSimulatorArm64::GetCReturnInt64() const {
-  DCHECK(kCanSimulate);
-  return simulator_->ReadXRegister(0);
+CodeSimulatorArm64::CodeSimulatorArm64() : BasicCodeSimulatorArm64() {}
+
+ARTSimulator* CodeSimulatorArm64::GetSimulator() {
+  return reinterpret_cast<ARTSimulator*>(instruction_simulator_.get());
 }
+
+Simulator* CodeSimulatorArm64::CreateNewInstructionSimulator(SimStack::Allocated&& stack) {
+  return new ARTSimulator(decoder_.get(), stdout, std::move(stack));
+}
+
+#endif
 
 }  // namespace arm64
 }  // namespace art
