d8587a939a0bef6481b166c3ad74aa56c60636e6
Chris Jones <christopher.jones@arm.com>
2024-01-05 16:30:59 +0000

    [Sim] Setup custom simulator
    Setup a custom ART simulator that can run Arm64 tests on an x86_64
    host machine. This patch does not support the actual running of the
    simulator but sets up the necessary components to enable this in
    future patches. Namely this patch does the following:
    
    - Refactor CodeSimulator and CodeSimulatorContainer classes to
      separate the semantics of basic and runtime-aware simulation.
    - Intercept branches to C++ entrypoints for the custom simulator.
      C++ entrypoint functions have a native context (are compiled as
      x86_64) and therefore there needs to be a transition in ISA/ABI
      when calling these functions from a quick context, i.e:
      entrypoints written in Arm64 assembly. To perform this transition,
      VIXL "runtime calls" will be used via the branch interception
      interface provided by VIXL.
    - Add a simulator version of the generic JNI trampoline to be used
      to handle the transition between native and quick code.
    
    Authors: Artem Serov <artem.serov@linaro.org>
             Greg Cawthorne <greg.cawthorne@linaro.org>
             Chris Jones <christopher.jones@arm.com>
    
    Test: test.py --host --target
    Change-Id: I7920cd376f9822fb866ecc6337b6cf4ff69c4cc2

--- runtime/oat/oat.h
+++ runtime/oat/oat.h
@@ -44,8 +44,8 @@ std::ostream& operator<<(std::ostream& stream, StubType stub_type);
 class EXPORT PACKED(4) OatHeader {
  public:
   static constexpr std::array<uint8_t, 4> kOatMagic { { 'o', 'a', 't', '\n' } };
-  // Last oat version changed reason: Restore to 16 KB ELF alignment.
-  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '5', '8', '\0'}};
+  // Last oat version changed reason: [Sim] Setup custom simulator.
+  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '5', '9', '\0'}};
 
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
   static constexpr const char* kDebuggableKey = "debuggable";
