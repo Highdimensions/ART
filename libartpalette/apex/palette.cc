dc55aa2f9f09e9cbc646a10f5954fbb8acf0d989
Hans Boehm <hboehm@google.com>
2024-11-14 10:58:18 -0800

    Add PaletteGetPriorityMapping
    ART sometimes needs access to the mapping itself, so that it can,
    for example, use an interpolated priority for system daemons.
    
    aosp/3287177 contains the current version of the proposed client code.
    
    Add PaletteClientTest for GetPriorityMapping.
    
    This is in preparation for allowing ART to further manipulate thread
    priorities. See go/thread-priorities-in-android and b/330444460 and
    its many friends.
    
    Driveby fix for PaletteDebugStoreGetString() in palette_fake.cc.
    It was fixed in the near-copy under system, but not here.
    
    Bug: 389104950
    Test: Treehugger
    Test: Treehugger in conection with aosp/3287177 and aosp/3286067
    Change-Id: Ic7ba204b2d25cb82c9db01b21389b958ae527793

--- libartpalette/apex/palette.cc
+++ libartpalette/apex/palette.cc
@@ -254,11 +254,18 @@ palette_status_t PaletteSetTaskProfiles(int32_t tid,
   return m(tid, profiles, profiles_len);
 }
 
-// Methods in version 4 API, corresponding to SDK level 36.
+// Introduced in version 4 API, corresponding to SDK level 36.
 palette_status_t PaletteDebugStoreGetString(char* result, size_t max_size) {
   PaletteDebugStoreGetStringMethod m =
       PaletteLoader::Instance().GetPaletteDebugStoreGetStringMethod();
   return m(result, max_size);
+}
+
+// Introduced in version 5 API, corresponding to SDK level 37.
+palette_status_t PaletteGetPriorityMapping(int32_t managed_priority, int* result) {
+  PaletteGetPriorityMappingMethod m =
+      PaletteLoader::Instance().GetPaletteGetPriorityMappingMethod();
+  return m(managed_priority, result);
 }
 
 }  // extern "C"
