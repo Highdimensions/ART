10e2816375f147d610a4cb2429b5ff868fb9b9fc
Almaz Mingaleev <mingaleev@google.com>
2024-11-28 15:20:10 +0000

    Call target method in accessor MHs when it is set.
    Bug: 297147201
    Test: ./art/tools/run-libcore-tests.sh --mode host --variant X64
    Test: ./art/test/testrunner/testrunner.py -b --host --64
    Test: ./art/test/testrunner/testrunner.py -b --target --64
    
    Change-Id: I5f8dc0761abbc1f5bb4f5727ae1e6966a4ebaf8a

--- runtime/mirror/method_handle_impl.h
+++ runtime/mirror/method_handle_impl.h
@@ -22,6 +22,7 @@
 #include "base/macros.h"
 #include "class.h"
 #include "method_type.h"
+#include "mirror/field.h"
 #include "obj_ptr.h"
 #include "object.h"
 
@@ -127,7 +128,19 @@ class MANAGED MethodHandleImpl : public MethodHandle {
                                                         Handle<MethodType> method_type)
       REQUIRES_SHARED(Locks::mutator_lock_) REQUIRES(!Roles::uninterruptible_);
 
+  EXPORT static ObjPtr<mirror::MethodHandleImpl> Create(Thread* const self,
+                                                        Handle<Field> field,
+                                                        MethodHandle::Kind kind,
+                                                        Handle<MethodType> method_type)
+      REQUIRES_SHARED(Locks::mutator_lock_) REQUIRES(!Roles::uninterruptible_);
+
+  static MemberOffset TargetOffset() {
+    return MemberOffset(OFFSETOF_MEMBER(MethodHandleImpl, target_));
+  }
+
  private:
+  HeapReference<mirror::Field> field_;
+  uint64_t target_;
   HeapReference<mirror::Object> target_class_or_info_;  // Unused by the runtime.
 
   friend struct art::MethodHandleImplOffsets;  // for verifying offset information
