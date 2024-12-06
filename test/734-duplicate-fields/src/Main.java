e894b07828e2ed8b6c2da1c4fec9ddee93fd8ddf
Santiago Aboy Solanes <solanes@google.com>
2024-12-06 12:29:53 +0000

    Make ResolveField respect the is_static parameter.
    Also fixes some logic and a DCHECK in the presence of duplicate
    fields.
    
    Bug: 364876321
    Test: 734-duplicate-fields
    Change-Id: Ice510db35853e6a3383e30393f616c839faca75d

---
+++ test/734-duplicate-fields/src/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+  public static void assertEquals(Object expected, Object actual) {
+    if (!expected.equals(actual)) {
+      throw new Error("Expected " + expected + ", got " + actual);
+    }
+  }
+
+  public static void assertEquals(String expected, String actual) {
+    if (!expected.equals(actual)) {
+      throw new Error("Expected \"" + expected + "\", got \"" + actual + "\"");
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Class<?> cls = Class.forName("Cls");
+    Object obj = cls.newInstance();
+    Object instanceResult = cls.getDeclaredMethod("getInstanceField").invoke(obj, null);
+    try {
+      cls.getDeclaredMethod("getStaticField").invoke(null, null);
+      throw new Error("Expected IncompatibleClassChangeError");
+    } catch (InvocationTargetException e) {
+      // Expected.
+      assertEquals(e.getCause().getClass(), IncompatibleClassChangeError.class);
+    }
+
+    assertEquals(0x42, instanceResult);
+    assertEquals("public int Cls.myField", cls.getDeclaredField("myField").toString());
+  }
+}
