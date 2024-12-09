f2d748428f5f55a7cc59a38f85178737e53c9d5e
Almaz Mingaleev <mingaleev@google.com>
2024-12-09 15:49:25 +0000

    Handle invoke-virtual targeting invokeExact gracefully.
    The interpreter handles such invoke-virtual as if they are native
    method and executes MethodHandle_invokeExact, which throws UOE.
    
    Bug: 383057088
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --interpreter
    Change-Id: I4dd4dea46eddd2b26e5866c80548b530a603d9c9

--- compiler/optimizing/inliner.cc
+++ compiler/optimizing/inliner.cc
@@ -1342,6 +1342,10 @@ bool HInliner::TryDevirtualize(HInvoke* invoke_instruction,
     return false;
   }
 
+  if (invoke_instruction->IsInvokeVirtual() && method->IsSignaturePolymorphic()) {
+    return false;
+  }
+
   // Don't devirtualize to an intrinsic invalid after the builder phase. The ArtMethod might be an
   // intrinsic even when the HInvoke isn't e.g. java.lang.CharSequence.isEmpty (not an intrinsic)
   // can get devirtualized into java.lang.String.isEmpty (which is an intrinsic).
