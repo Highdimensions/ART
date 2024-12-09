f6630d871ea2e1d0a59e5936730d35e108e4cac5
Almaz Mingaleev <mingaleev@google.com>
2024-12-09 15:49:25 +0000

    Handle invoke-virtual targeting invokeExact gracefully.
    The interpreter handles such invoke-virtual as if they are native
    method and executes MethodHandle_invokeExact, which throws UOE.
    
    Bug: 383057088
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --interpreter
    Change-Id: I4dd4dea46eddd2b26e5866c80548b530a603d9c9

--- compiler/optimizing/instruction_builder.cc
+++ compiler/optimizing/instruction_builder.cc
@@ -32,6 +32,7 @@
 #include "handle_cache-inl.h"
 #include "imtable-inl.h"
 #include "intrinsics.h"
+#include "intrinsics_enum.h"
 #include "intrinsics_utils.h"
 #include "jit/jit.h"
 #include "jit/profiling_info.h"
@@ -1185,6 +1186,14 @@ bool HInstructionBuilder::BuildInvoke(const Instruction& instruction,
       invoke->SetArgumentAt(clinit_check_index, clinit_check);
     }
   } else if (invoke_type == kVirtual) {
+    bool enable_intrinsic_opt = !graph_->IsDebuggable();
+    if (resolved_method->IsIntrinsic()) {
+      Intrinsics intrinsic = resolved_method->GetIntrinsic();
+      if (intrinsic == Intrinsics::kMethodHandleInvokeExact ||
+          intrinsic == Intrinsics::kMethodHandleInvoke) {
+        enable_intrinsic_opt = false;
+      }
+    }
     invoke = new (allocator_) HInvokeVirtual(allocator_,
                                              number_of_arguments,
                                              operands.GetNumberOfOperands(),
@@ -1194,7 +1203,7 @@ bool HInstructionBuilder::BuildInvoke(const Instruction& instruction,
                                              resolved_method,
                                              resolved_method_reference,
                                              /*vtable_index=*/ imt_or_vtable_index,
-                                             !graph_->IsDebuggable());
+                                             enable_intrinsic_opt);
   } else {
     DCHECK_EQ(invoke_type, kInterface);
     if (kIsDebugBuild) {
---
+++ test/2286-invokevirtual-invokeexact/src/Main.java
@@ -0,0 +1,27 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        MethodHandle mh = MethodHandles.lookup()
+            .findStatic(Main.class, "toString", MethodType.methodType(Object.class, Object[].class));
+
+        Object[] objects = new Object[2];
+        objects[0] = "111";
+        objects[1] = 10;
+
+        Class testClass = Class.forName("Test");
+
+        Method m = testClass.getDeclaredMethod("test", MethodHandle.class, Object[].class);
+        try {
+            Object o = m.invoke(null, mh, objects);
+            throw new AssertionError("unreachable");
+        } catch (Exception expected) {}
+    }
+
+    static Object toString(Object[] objects) {
+        return "objects";
+    }
+}
