cea5f0db60beab98ec7783b874f496fe6433e671
Almaz Mingaleev <mingaleev@google.com>
2024-12-09 15:49:25 +0000

    Handle invoke-virtual targeting invokeExact gracefully.
    The interpreter handles such invoke-virtual as if they are native
    method and executes MethodHandle_invokeExact, which throws UOE.
    
    Bug: 383057088
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Test: ./art/test/testrunner/testrunner.py --host --64 -b --interpreter
    Change-Id: I4dd4dea46eddd2b26e5866c80548b530a603d9c9

--- compiler/optimizing/instruction_builder.cc
+++ compiler/optimizing/instruction_builder.cc
@@ -32,6 +32,7 @@
 #include "handle_cache-inl.h"
 #include "imtable-inl.h"
 #include "intrinsics.h"
+#include "intrinsics_enum.h"
 #include "intrinsics_utils.h"
 #include "jit/jit.h"
 #include "jit/profiling_info.h"
@@ -1051,6 +1052,23 @@ static ArtMethod* ResolveMethod(uint16_t method_idx,
   return resolved_method;
 }
 
+static bool IsSignaturePolymorphic(ArtMethod* method) {
+  if (!method->IsIntrinsic()) {
+    return false;
+  }
+  Intrinsics intrinsic = method->GetIntrinsic();
+
+  switch (intrinsic) {
+#define IS_POLYMOPHIC(Name, ...) \
+    case Intrinsics::k ## Name:
+      ART_SIGNATURE_POLYMORPHIC_INTRINSICS_LIST(IS_POLYMOPHIC)
+#undef IS_POLYMOPHIC
+      return true;
+    default:
+      return false;
+  }
+}
+
 bool HInstructionBuilder::BuildInvoke(const Instruction& instruction,
                                       uint32_t dex_pc,
                                       uint32_t method_idx,
@@ -1189,16 +1207,36 @@ bool HInstructionBuilder::BuildInvoke(const Instruction& instruction,
       invoke->SetArgumentAt(clinit_check_index, clinit_check);
     }
   } else if (invoke_type == kVirtual) {
-    invoke = new (allocator_) HInvokeVirtual(allocator_,
-                                             number_of_arguments,
-                                             operands.GetNumberOfOperands(),
-                                             return_type,
-                                             dex_pc,
-                                             method_reference,
-                                             resolved_method,
-                                             resolved_method_reference,
-                                             /*vtable_index=*/ imt_or_vtable_index,
-                                             !graph_->IsDebuggable());
+    // In the wild there are apps which have invoke-virtual targeting polymorphic methods like
+    // MethodHandle.invokeExact.
+    // It never worked in the first place, but invokeExact intrinsics expect invokeExact to be
+    // called using invoke-polymorphic and would throw an exception otherwise. This forces runtime
+    // to treat such invoke-virtual calls as-if they are calling a native method, which
+    // MethodHandle's invoke and invokeExact are.
+    if (kIsDebugBuild) {
+      ScopedObjectAccess soa(Thread::Current());
+      CHECK_EQ(IsSignaturePolymorphic(resolved_method), resolved_method->IsSignaturePolymorphic());
+    }
+    if (IsSignaturePolymorphic(resolved_method)) {
+      invoke = new (allocator_) HInvokeUnresolved(allocator_,
+                                                  number_of_arguments,
+                                                  operands.GetNumberOfOperands(),
+                                                  return_type,
+                                                  dex_pc,
+                                                  method_reference,
+                                                  InvokeType::kVirtual);
+    } else {
+      invoke = new (allocator_) HInvokeVirtual(allocator_,
+                                               number_of_arguments,
+                                               operands.GetNumberOfOperands(),
+                                               return_type,
+                                               dex_pc,
+                                               method_reference,
+                                               resolved_method,
+                                               resolved_method_reference,
+                                               /*vtable_index=*/ imt_or_vtable_index,
+                                               !graph_->IsDebuggable());
+    }
   } else {
     DCHECK_EQ(invoke_type, kInterface);
     if (kIsDebugBuild) {
