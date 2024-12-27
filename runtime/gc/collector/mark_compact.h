d3e83b8e9a7049adacc3f81261ab654fa6824d7d
Lokesh Gidra <lokeshgidra@google.com>
2024-12-27 09:49:23 +0000

    Use MOVE (uffd) ioctl for mapping compacted pages
    Currently we use COPY ioctl to map compacted pages. But it incurs high
    overhead as the page(s) are allocated in the kernel, memset zeroed, and then
    memcpy'ed before mapping them. On the other hand, MOVE ioctl avoids all
    of this and directly maps the soure page. This also enables us to
    recycle the from-space pages in userspace, thereby eliminating the need
    for madvise.
    
    NOTE: Black-dense pages will still use COPY ioctl. A subsequent CL will
    cover them.
    
    Bug: 343220989
    Test: art/test/testrunner/testrunner.py --host
    Change-Id: I3bac4093d226334fc30230c5ba3481184dcc987a

--- runtime/gc/collector/mark_compact.h
+++ runtime/gc/collector/mark_compact.h
@@ -117,7 +117,7 @@ class MarkCompact final : public GarbageCollector {
   using SigbusCounterType = uint32_t;
 
   static constexpr size_t kAlignment = kObjectAlignment;
-  static constexpr int kCopyMode = -1;
+  static constexpr int kUffdMode = -1;
   // Fake file descriptor for fall back mode (when uffd isn't available)
   static constexpr int kFallbackMode = -3;
   static constexpr int kFdUnused = -2;
@@ -141,6 +141,7 @@ class MarkCompact final : public GarbageCollector {
   // Called by SIGBUS handler. NO_THREAD_SAFETY_ANALYSIS for mutator-lock, which
   // is asserted in the function.
   bool SigbusHandler(siginfo_t* info) REQUIRES(!lock_) NO_THREAD_SAFETY_ANALYSIS;
+  bool SigsysHandler(siginfo_t* info, void* context) REQUIRES(!lock_) NO_THREAD_SAFETY_ANALYSIS;
 
   GcType GetGcType() const override { return kGcTypePartial; }
 
@@ -640,6 +641,8 @@ class MarkCompact final : public GarbageCollector {
   // returns. Returns number of bytes (multiple of page-size) mapped.
   size_t CopyIoctl(
       void* dst, void* buffer, size_t length, bool return_on_contention, bool tolerate_enoent);
+  // Move 'len/page-size' pages from 'src' to 'dst'.
+  size_t MoveIoctl(void* dst, void* src, size_t len, bool tolerate_enoent);
 
   // Called after updating linear-alloc page(s) to map the page. It first
   // updates the state of the pages to kProcessedAndMapping and after ioctl to
@@ -680,6 +683,10 @@ class MarkCompact final : public GarbageCollector {
   // Scan old-gen for young GCs by looking for cards that are at least 'aged' in
   // the card-table corresponding to moving and non-moving spaces.
   void ScanOldGenObjects() REQUIRES(Locks::heap_bitmap_lock_) REQUIRES_SHARED(Locks::mutator_lock_);
+  // Return a free page from 'from-space' that can be used to copy objects into
+  // and then passed onto userfaultfd ioctls. Return nullptr if no page is
+  // available. Size must be a multiple of page-size.
+  uint8_t* GetFreePageForMapping(size_t size, bool atomic);
 
   // For checkpoints
   Barrier gc_barrier_;
@@ -789,10 +796,10 @@ class MarkCompact final : public GarbageCollector {
   // All the pages in [last_reclaimable_page_, last_reclaimed_page_) in
   // from-space are available to store compacted contents for batching until the
   // next time madvise is called.
-  uint8_t* last_reclaimable_page_;
+  volatile uint8_t* last_reclaimable_page_;
   // [cur_reclaimable_page_, last_reclaimed_page_) have been used to store
   // compacted contents for batching.
-  uint8_t* cur_reclaimable_page_;
+  std::atomic<uint8_t*> cur_reclaimable_page_;
 
   // Mark bits for non-moving space
   accounting::ContinuousSpaceBitmap* non_moving_space_bitmap_;
@@ -841,6 +848,7 @@ class MarkCompact final : public GarbageCollector {
   // Set to true when doing young gen collection.
   bool young_gen_;
   const bool use_generational_;
+  bool use_move_ioctl_;
   // True while compacting.
   bool compacting_;
   // Mark bits for main space
@@ -899,7 +907,6 @@ class MarkCompact final : public GarbageCollector {
   // END HOT FIELDS: accessed per reference update
   // END HOT FIELDS: accessed per object
 
-  uint8_t* conc_compaction_termination_page_;
   PointerSize pointer_size_;
   // Userfault file descriptor, accessed only by the GC itself.
   // kFallbackMode value indicates that we are in the fallback mode.
