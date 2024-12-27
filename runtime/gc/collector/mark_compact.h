18a05a69f34e76586c58ff5f02dfb3dd106980bd
Lokesh Gidra <lokeshgidra@google.com>
2024-12-27 09:49:23 +0000

    Use MOVE (uffd) ioctl for mapping compacted pages
    Currently we use COPY ioctl to map compacted pages. But it incurs high
    overhead as the page(s) are allocated in the kernel, memset zeroed, and then
    memcpy'ed before mapping them. On the other hand, MOVE ioctl avoids all
    of this and directly maps the soure page. This also enables us to
    recycle the from-space pages in userspace, thereby eliminating the need
    for madvise.
    
    NOTE: Black-dense pages will still use COPY ioctl. A subsequent CL will
    do that.
    
    Bug: 343220989
    Test: art/test/testrunner/testrunner.py --host
    Change-Id: I3bac4093d226334fc30230c5ba3481184dcc987a

--- runtime/gc/collector/mark_compact.h
+++ runtime/gc/collector/mark_compact.h
@@ -619,6 +619,8 @@ class MarkCompact final : public GarbageCollector {
   // returns. Returns number of bytes (multiple of page-size) mapped.
   size_t CopyIoctl(
       void* dst, void* buffer, size_t length, bool return_on_contention, bool tolerate_enoent);
+  // Move 'len/page-size' pages from 'src' to 'dst'.
+  size_t MoveIoctl(void* dst, void* src, size_t len, bool tolerate_enoent);
 
   // Called after updating linear-alloc page(s) to map the page. It first
   // updates the state of the pages to kProcessedAndMapping and after ioctl to
@@ -885,6 +887,7 @@ class MarkCompact final : public GarbageCollector {
   // When using SIGBUS feature, this counter is used by mutators to claim a page
   // out of compaction buffers to be used for the entire compaction cycle.
   std::atomic<uint16_t> compaction_buffer_counter_;
+  bool use_move_ioctl_;
   // Set to true in MarkingPause() to indicate when allocation_stack_ should be
   // checked in IsMarked() for black allocations.
   bool marking_done_;
