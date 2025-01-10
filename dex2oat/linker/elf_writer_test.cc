e38ac7cf9806191ac43e0a2ac2fcb2fe61c21fcc
Konstantin Baladurin <konstantin.baladurin@arm.com>
2025-01-10 14:07:53 +0000

    [RFC] Move dynamic sections to start of OAT file.
    NOT FOR MERGING.
    
    Extra padding is required in OAT files when a section have different
    runtime permissions from the previous one, because in this case
    it should be mmaped separately as a part of a new loadable segment
    and therefore it should be page-aligned in the file.
    
    This patch places read-only dynamic sections prior to the read-only
    .rodata section (before it), thus combining a few R sections into
    a segment and reducing the amount of padding required.
    
    To do this:
    
    * Change runtime permissions of .dynamic section from read-write to
      read-only. This is valid because:
      * Dynamic linker can update only DT_DEBUG entry that we don't use
      * ELF spec does not require it to be writable. Rather, it states
        that it can be writable depending on the OS and processor.
    
      However, glibc versions <= 2.34 have a bug that causes a crash
      when loading a dynamic shared object with read-only .dynamic section
      on host: (https://sourceware.org/bugzilla/show_bug.cgi?id=28340).
      Therefore only use glibc's dlopen when it is version > 2.34, otherwise
      use ART's ELF loader. Bionic dlopen correctly accepts read-only
      .dynamic section.
    
    * Put the dynamic sections (.dynstr, .dynsym, .hash, .dynamic) prior
      to the .rodata section. As content of dynamic sections depend on
      other sections, we firstly reserve enough space for them before
      .rodata and then generate actual data and write it.
    
    Actual disk usage reduction from this patch:
    * Approx 90% of tested dense .odex files are reduced by 40 KiB
      (all reductions are in range 16-48 KiB).
    * Approx 95% of tested sparse .odex files are reduced by 8 KiB
      (all reductions are in range 4-16 Kib).
    
    This patch, along with the patch that reduces .rodata alignment,
    decreases the total sparse size of oat files from system.img
    built for aosp_shiba-trunk_staging-user lunch target by 1.4%
    (from ~222 MiB to ~219 MiB).
    
    Test: testrunner.py --optimizing
    
    Bug: 378792349
    
    Change-Id: I2a4929adfdf8a496832880f42293371ae2e0e06b

--- dex2oat/linker/elf_writer_test.cc
+++ dex2oat/linker/elf_writer_test.cc
@@ -21,6 +21,7 @@
 #include "base/unix_file/fd_file.h"
 #include "base/utils.h"
 #include "common_compiler_driver_test.h"
+#include "driver/compiler_driver.h"
 #include "elf/elf_builder.h"
 #include "elf_writer_quick.h"
 #include "oat/elf_file.h"
@@ -35,6 +36,50 @@ class ElfWriterTest : public CommonCompilerDriverTest {
   void SetUp() override {
     ReserveImageSpace();
     CommonCompilerTest::SetUp();
+    CreateCompilerDriver();
+  }
+
+  void WriteElf(File* oat_file,
+                const std::vector<uint8_t>& rodata,
+                const std::vector<uint8_t>& text,
+                const std::vector<uint8_t>& data_img_rel_ro,
+                size_t data_img_rel_ro_app_image_offset,
+                size_t bss_size,
+                size_t bss_methods_offset,
+                size_t bss_roots_offset,
+                size_t dex_section_size) {
+    std::unique_ptr<ElfWriter> elf_writer = CreateElfWriterQuick(
+      compiler_driver_->GetCompilerOptions(),
+      oat_file);
+
+    elf_writer->Start();
+    OutputStream* rodata_section = elf_writer->StartRoData();
+
+    elf_writer->PrepareDynamicSection(rodata.size(),
+                                      text.size(),
+                                      data_img_rel_ro.size(),
+                                      data_img_rel_ro_app_image_offset,
+                                      bss_size,
+                                      bss_methods_offset,
+                                      bss_roots_offset,
+                                      dex_section_size);
+
+    ASSERT_TRUE(rodata_section->WriteFully(rodata.data(), rodata.size()));
+    elf_writer->EndRoData(rodata_section);
+
+    OutputStream* text_section = elf_writer->StartText();
+    ASSERT_TRUE(text_section->WriteFully(text.data(), text.size()));
+    elf_writer->EndText(text_section);
+
+    if (!data_img_rel_ro.empty()) {
+      OutputStream* data_img_rel_ro_section = elf_writer->StartDataImgRelRo();
+      ASSERT_TRUE(data_img_rel_ro_section->WriteFully(data_img_rel_ro.data(),
+          data_img_rel_ro.size()));
+      elf_writer->EndDataImgRelRo(data_img_rel_ro_section);
+    }
+
+    elf_writer->WriteDynamicSection();
+    ASSERT_TRUE(elf_writer->End());
   }
 };
 
@@ -134,6 +179,216 @@ TEST_F(ElfWriterTest, CheckBuildIdPresent) {
                                               &error_msg));
     CHECK(ef.get() != nullptr) << error_msg;
     EXPECT_TRUE(ef->HasSection(".note.gnu.build-id"));
+  }
+}
+
+TEST_F(ElfWriterTest, CheckDynamicSection) {
+  auto verify = [this](size_t rodata_size,
+                       size_t text_size,
+                       size_t data_img_rel_ro_size,
+                       size_t data_img_rel_ro_app_image_offset,
+                       size_t bss_size,
+                       size_t bss_methods_offset,
+                       size_t bss_roots_offset,
+                       size_t dex_section_size) {
+    SCOPED_TRACE(testing::Message() << "rodata_size: " << rodata_size
+                 << ", text_size: " << text_size
+                 << ", data_img_rel_ro_size: " << data_img_rel_ro_size
+                 << ", data_img_rel_ro_app_image_offset: " << data_img_rel_ro_app_image_offset
+                 << ", bss_size: " << bss_size
+                 << ", bss_methods_offset: " << bss_methods_offset
+                 << ", bss_roots_offset: " << bss_roots_offset
+                 << ", dex_section_size: " << dex_section_size);
+
+    std::vector<uint8_t> rodata(rodata_size, 0xAA);
+    std::vector<uint8_t> text(text_size, 0xBB);
+    std::vector<uint8_t> data_img_rel_ro(data_img_rel_ro_app_image_offset, 0xCC);
+    size_t data_img_rel_ro_app_image_size = data_img_rel_ro_size - data_img_rel_ro_app_image_offset;
+    data_img_rel_ro.insert(data_img_rel_ro.cend(), data_img_rel_ro_app_image_size, 0xDD);
+
+    ScratchFile tmp_base, tmp_oat(tmp_base, ".oat");
+    WriteElf(tmp_oat.GetFile(),
+             rodata,
+             text,
+             data_img_rel_ro,
+             data_img_rel_ro_app_image_offset,
+             bss_size,
+             bss_methods_offset,
+             bss_roots_offset,
+             dex_section_size);
+
+    std::string error_msg;
+    std::unique_ptr<ElfFile> ef(ElfFile::Open(tmp_oat.GetFile(),
+                                              /*writable=*/ false,
+                                              /*program_header_only=*/ true,
+                                              /*low_4gb=*/ false,
+                                              &error_msg));
+    ASSERT_NE(ef.get(), nullptr) << error_msg;
+    ASSERT_TRUE(ef->Load(tmp_oat.GetFile(),
+                         /*executable=*/false,
+                         /*low_4gb=*/false,
+                         /*reservation=*/nullptr,
+                         &error_msg)) << error_msg;
+
+    const uint8_t* oatdata_ptr = ef->FindDynamicSymbolAddress("oatdata");
+    ASSERT_NE(oatdata_ptr, nullptr);
+    EXPECT_EQ(memcmp(oatdata_ptr, rodata.data(), rodata.size()), 0);
+
+    size_t page_size = GetPageSizeSlow();
+    size_t elf_word_size = ef->Is64Bit() ? sizeof(ElfTypes64::Word) : sizeof(ElfTypes32::Word);
+
+    if (text_size != 0u) {
+      const uint8_t* text_ptr = ef->FindDynamicSymbolAddress("oatexec");
+      ASSERT_NE(text_ptr, nullptr);
+      ASSERT_TRUE(IsAlignedParam(text_ptr, page_size));
+      EXPECT_EQ(memcmp(text_ptr, text.data(), text.size()), 0);
+
+      const uint8_t* oatlastword_ptr = ef->FindDynamicSymbolAddress("oatlastword");
+      ASSERT_NE(oatlastword_ptr, nullptr);
+      EXPECT_EQ(static_cast<size_t>(oatlastword_ptr - text_ptr), text_size - elf_word_size);
+    } else if (rodata_size != 0u) {
+      const uint8_t* oatlastword_ptr = ef->FindDynamicSymbolAddress("oatlastword");
+      ASSERT_NE(oatlastword_ptr, nullptr);
+      EXPECT_EQ(static_cast<size_t>(oatlastword_ptr - oatdata_ptr), rodata_size - elf_word_size);
+    }
+
+    if (data_img_rel_ro_size != 0u) {
+      const uint8_t* oatdataimgrelro_ptr = ef->FindDynamicSymbolAddress("oatdataimgrelro");
+      ASSERT_NE(oatdataimgrelro_ptr, nullptr);
+      ASSERT_TRUE(IsAlignedParam(oatdataimgrelro_ptr, page_size));
+      EXPECT_EQ(memcmp(oatdataimgrelro_ptr, data_img_rel_ro.data(), data_img_rel_ro.size()), 0);
+
+      const uint8_t* oatdataimgrelrolastword_ptr =
+          ef->FindDynamicSymbolAddress("oatdataimgrelrolastword");
+      ASSERT_NE(oatdataimgrelrolastword_ptr, nullptr);
+      EXPECT_EQ(static_cast<size_t>(oatdataimgrelrolastword_ptr - oatdataimgrelro_ptr),
+          data_img_rel_ro_size - elf_word_size);
+
+      if (data_img_rel_ro_app_image_offset != data_img_rel_ro_size) {
+        const uint8_t* oatdataimgrelroappimage_ptr =
+            ef->FindDynamicSymbolAddress("oatdataimgrelroappimage");
+        ASSERT_NE(oatdataimgrelroappimage_ptr, nullptr);
+        EXPECT_EQ(static_cast<size_t>(oatdataimgrelroappimage_ptr - oatdataimgrelro_ptr),
+          data_img_rel_ro_app_image_offset);
+      }
+
+      if (bss_size != 0u) {
+        const uint8_t* bss_ptr = ef->FindDynamicSymbolAddress("oatbss");
+        ASSERT_NE(bss_ptr, nullptr);
+        ASSERT_TRUE(IsAlignedParam(bss_ptr, page_size));
+
+        if (bss_methods_offset != bss_roots_offset) {
+          const uint8_t* oatbssmethods_ptr = ef->FindDynamicSymbolAddress("oatbssmethods");
+          ASSERT_NE(oatbssmethods_ptr, nullptr);
+          EXPECT_EQ(static_cast<size_t>(oatbssmethods_ptr - bss_ptr), bss_methods_offset);
+        }
+
+        if (bss_roots_offset != bss_size) {
+          const uint8_t* oatbssroots_ptr = ef->FindDynamicSymbolAddress("oatbssroots");
+          ASSERT_NE(oatbssroots_ptr, nullptr);
+          EXPECT_EQ(static_cast<size_t>(oatbssroots_ptr - bss_ptr), bss_roots_offset);
+        }
+
+        const uint8_t* oatbsslastword_ptr = ef->FindDynamicSymbolAddress("oatbsslastword");
+        ASSERT_NE(oatbsslastword_ptr, nullptr);
+        EXPECT_EQ(static_cast<size_t>(oatbsslastword_ptr - bss_ptr), bss_size - elf_word_size);
+      }
+
+      if (dex_section_size != 0u) {
+        const uint8_t* dex_ptr = ef->FindDynamicSymbolAddress("oatdex");
+        ASSERT_NE(dex_ptr, nullptr);
+        ASSERT_TRUE(IsAlignedParam(dex_ptr, page_size));
+        const uint8_t* oatdexlastword_ptr = ef->FindDynamicSymbolAddress("oatdexlastword");
+        EXPECT_EQ(static_cast<size_t>(oatdexlastword_ptr - dex_ptr),
+            dex_section_size - elf_word_size);
+      }
+    }
+  };
+
+  enum class Symbol {
+    kRodata,
+    kText,
+    kDataImgRelRo,
+    kDataImgRelRoAppImage,
+    kBss,
+    kBssMethods,
+    kBssRoots,
+    kDex,
+    kLast = kDex
+  };
+
+  constexpr size_t kNumberOfSymbols = static_cast<size_t>(Symbol::kLast) + 1;
+
+  // Use an unaligned section size to verify that ElfWriter properly aligns sections in this case.
+  // We can use an arbitrary value that is greater than or equal to an ElfWord (4 bytes).
+  constexpr size_t kSectionSize = 127u;
+  // Offset in .data.img.rel.ro section from its beginning. We can use any value in the range
+  // [0, kSectionSize).
+  constexpr size_t kDataImgRelRoAppImageOffset = kSectionSize / 2;
+  // Offsets in .bss from its beginning. We can use any value in the range [0, kSectionSize),
+  // kBssMethodsOffset should be less than or equal to kBssRootsOffset.
+  constexpr size_t kBssMethodsOffset = kSectionSize / 3;
+  constexpr size_t kBssRootsOffset = 2 * kBssMethodsOffset;
+
+  auto exists = [](Symbol symbol, const std::bitset<kNumberOfSymbols> &symbols) {
+    return symbols.test(static_cast<size_t>(symbol));
+  };
+
+  auto get_size = [&](Symbol symbol, const std::bitset<kNumberOfSymbols> &symbols) -> size_t {
+    return exists(symbol, symbols) ? kSectionSize : 0;
+  };
+
+  auto get_offset = [&](Symbol symbol, const std::bitset<kNumberOfSymbols> &symbols) -> size_t {
+    if (symbol == Symbol::kDataImgRelRoAppImage) {
+      return exists(symbol, symbols) ? kDataImgRelRoAppImageOffset : 0u;
+    }
+    if (symbol == Symbol::kBssMethods) {
+      if (!exists(Symbol::kBss, symbols)) {
+        return 0u;
+      }
+      if (exists(symbol, symbols)) {
+        return kBssMethodsOffset;
+      }
+      if (exists(Symbol::kBssRoots, symbols)) {
+        return kBssRootsOffset;
+      }
+      return kSectionSize;
+    }
+    if (symbol == Symbol::kBssRoots) {
+      if (!exists(Symbol::kBss, symbols)) {
+        return 0u;
+      }
+      return exists(symbol, symbols) ? kBssRootsOffset : kSectionSize;
+    }
+    LOG(FATAL) << "Unreachable";
+    UNREACHABLE();
+  };
+
+  auto is_valid_mask = [&](const std::bitset<kNumberOfSymbols> &symbols) {
+    if (!exists(Symbol::kDataImgRelRo, symbols)) {
+      return !exists(Symbol::kDataImgRelRoAppImage, symbols);
+    }
+    if (!exists(Symbol::kBss, symbols)) {
+      return !exists(Symbol::kBssMethods, symbols) && !exists(Symbol::kBssRoots, symbols);
+    }
+    return true;
+  };
+
+  size_t last_mask = (1 << kNumberOfSymbols) - 1;
+
+  for (size_t mask = 0; mask <= last_mask; mask++) {
+    std::bitset<kNumberOfSymbols> symbols(mask);
+    if (!is_valid_mask(symbols)) {
+      continue;
+    }
+    verify(get_size(Symbol::kRodata, symbols),
+           get_size(Symbol::kText, symbols),
+           get_size(Symbol::kDataImgRelRo, symbols),
+           get_offset(Symbol::kDataImgRelRoAppImage, symbols),
+           get_size(Symbol::kBss, symbols),
+           get_offset(Symbol::kBssMethods, symbols),
+           get_offset(Symbol::kBssRoots, symbols),
+           get_size(Symbol::kDex, symbols));
   }
 }
 
