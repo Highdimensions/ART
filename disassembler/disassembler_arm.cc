bb9eef48ae2e6bfc34affca9b4c760356f08921d
Elliott Hughes <enh@google.com>
2025-01-13 13:26:32 -0800

    Add an explicit destructor for DisassemblerArm.
    The std::unique_ptr destructor is constexpr in C++23, which means clang catches more attempts to delete in incomplete type than it used to.
    
    Change-Id: I8e2c953b28d5915b8dbd448e4d7cf08582d9cf0c

--- disassembler/disassembler_arm.cc
+++ disassembler/disassembler_arm.cc
@@ -218,7 +218,10 @@ void DisassemblerArm::CustomDisassembler::CustomDisassemblerStream::PrintLiteral
 }
 
 DisassemblerArm::DisassemblerArm(DisassemblerOptions* options)
-    : Disassembler(options), disasm_(std::make_unique<CustomDisassembler>(output_, options)) {}
+    : Disassembler(options), disasm_(new CustomDisassembler(output_, options)) {}
+
+DisassemblerArm::~DisassemblerArm() {
+}
 
 size_t DisassemblerArm::Dump(std::ostream& os, const uint8_t* begin) {
   uintptr_t next;
