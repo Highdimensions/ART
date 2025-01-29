c19080432bc047dc6f739f4c4c3f7de675424be8
dimitry <dimitry@google.com>
2025-01-29 13:24:43 +0100

    nativebridge: Add isFunctionPointerExecutableWithNativeBridge method
    This method lets native bridge report if particular pointer is in the
    region of code executable by native bridge implementation.
    
    Bug: 393035780
    Test: atest art/libnativebridge/tests
    Change-Id: Icaec80cb7efae5b918aed0a98216cbfaeaba330d

---
+++ libnativebridge/tests/NativeBridge8IdentifyTrampolines_lib.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "NativeBridge8InetifyTrampolines.h"
+
+namespace android {
+
+static const void* g_is_function_pointer_executable_with_native_bridge_called_with = nullptr;
+
+void SetIsFunctionPointerExecutableWithNativeBridgeCalled(const void* ptr) {
+  g_is_function_pointer_executable_with_native_bridge_called_with = ptr;
+}
+
+bool IsFunctionPointerExecutableWithNativeBridgeCalledFor(const void* ptr) {
+  return g_is_function_pointer_executable_with_native_bridge_called_with == ptr;
+}
+
+}  // namespace android
