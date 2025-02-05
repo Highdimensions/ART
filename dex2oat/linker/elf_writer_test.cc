0886e11363b24dd700403b087ee98201295186ef
Jiakai Zhang <jiakaiz@google.com>
2025-02-05 18:23:42 +0000

    Remove more dead code for ELF file.
    `writable` is always false. `program_header_only` is always true except
    for in some tests.
    
    Bug: 377474232
    Bug: 6527146
    Test: art/test.py --host -g
    Change-Id: Icc9b4dbae612a90f54b50e090e191fad0a19f709

--- dex2oat/linker/elf_writer_test.cc
+++ dex2oat/linker/elf_writer_test.cc
@@ -83,105 +83,6 @@ class ElfWriterTest : public CommonCompilerDriverTest {
   }
 };
 
-#define EXPECT_ELF_FILE_ADDRESS(ef, expected_value, symbol_name, build_map) \
-  do { \
-    void* addr = reinterpret_cast<void*>((ef)->FindSymbolAddress(SHT_DYNSYM, \
-                                                                 symbol_name, \
-                                                                 build_map)); \
-    EXPECT_NE(nullptr, addr); \
-    if ((expected_value) == nullptr) { \
-      (expected_value) = addr; \
-    }                        \
-    EXPECT_EQ(expected_value, addr); \
-    EXPECT_EQ(expected_value, (ef)->FindDynamicSymbolAddress(symbol_name)); \
-  } while (false)
-
-TEST_F(ElfWriterTest, dlsym) {
-  std::string elf_location = GetCoreOatLocation();
-  std::string elf_filename = GetSystemImageFilename(elf_location.c_str(), kRuntimeISA);
-  LOG(INFO) << "elf_filename=" << elf_filename;
-
-  UnreserveImageSpace();
-  void* dl_oatdata = nullptr;
-  void* dl_oatexec = nullptr;
-  void* dl_oatlastword = nullptr;
-
-  std::unique_ptr<File> file(OS::OpenFileForReading(elf_filename.c_str()));
-  ASSERT_TRUE(file.get() != nullptr) << elf_filename;
-  {
-    std::string error_msg;
-    std::unique_ptr<ElfFile> ef(ElfFile::Open(file.get(),
-                                              /*writable=*/ false,
-                                              /*program_header_only=*/ false,
-                                              /*low_4gb=*/false,
-                                              &error_msg));
-    CHECK(ef.get() != nullptr) << error_msg;
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatdata, "oatdata", false);
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatexec, "oatexec", false);
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatlastword, "oatlastword", false);
-  }
-  {
-    std::string error_msg;
-    std::unique_ptr<ElfFile> ef(ElfFile::Open(file.get(),
-                                              /*writable=*/ false,
-                                              /*program_header_only=*/ false,
-                                              /*low_4gb=*/ false,
-                                              &error_msg));
-    CHECK(ef.get() != nullptr) << error_msg;
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatdata, "oatdata", true);
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatexec, "oatexec", true);
-    EXPECT_ELF_FILE_ADDRESS(ef, dl_oatlastword, "oatlastword", true);
-  }
-  {
-    std::string error_msg;
-    std::unique_ptr<ElfFile> ef(ElfFile::Open(file.get(),
-                                              /*writable=*/ false,
-                                              /*program_header_only=*/ true,
-                                              /*low_4gb=*/ false,
-                                              &error_msg));
-    CHECK(ef.get() != nullptr) << error_msg;
-    size_t size;
-    bool success = ef->GetLoadedSize(&size, &error_msg);
-    CHECK(success) << error_msg;
-    MemMap reservation = MemMap::MapAnonymous("ElfWriterTest#dlsym reservation",
-                                              RoundUp(size, MemMap::GetPageSize()),
-                                              PROT_NONE,
-                                              /*low_4gb=*/ true,
-                                              &error_msg);
-    CHECK(reservation.IsValid()) << error_msg;
-    uint8_t* base = reservation.Begin();
-    success =
-        ef->Load(file.get(), /*executable=*/ false, /*low_4gb=*/ false, &reservation, &error_msg);
-    CHECK(success) << error_msg;
-    CHECK(!reservation.IsValid());
-    EXPECT_EQ(reinterpret_cast<uintptr_t>(dl_oatdata) + reinterpret_cast<uintptr_t>(base),
-        reinterpret_cast<uintptr_t>(ef->FindDynamicSymbolAddress("oatdata")));
-    EXPECT_EQ(reinterpret_cast<uintptr_t>(dl_oatexec) + reinterpret_cast<uintptr_t>(base),
-        reinterpret_cast<uintptr_t>(ef->FindDynamicSymbolAddress("oatexec")));
-    EXPECT_EQ(reinterpret_cast<uintptr_t>(dl_oatlastword) + reinterpret_cast<uintptr_t>(base),
-        reinterpret_cast<uintptr_t>(ef->FindDynamicSymbolAddress("oatlastword")));
-  }
-}
-
-TEST_F(ElfWriterTest, CheckBuildIdPresent) {
-  std::string elf_location = GetCoreOatLocation();
-  std::string elf_filename = GetSystemImageFilename(elf_location.c_str(), kRuntimeISA);
-  LOG(INFO) << "elf_filename=" << elf_filename;
-
-  std::unique_ptr<File> file(OS::OpenFileForReading(elf_filename.c_str()));
-  ASSERT_TRUE(file.get() != nullptr);
-  {
-    std::string error_msg;
-    std::unique_ptr<ElfFile> ef(ElfFile::Open(file.get(),
-                                              /*writable=*/ false,
-                                              /*program_header_only=*/ false,
-                                              /*low_4gb=*/ false,
-                                              &error_msg));
-    CHECK(ef.get() != nullptr) << error_msg;
-    EXPECT_TRUE(ef->HasSection(".note.gnu.build-id"));
-  }
-}
-
 // Check that dynamic sections (.dynamic, .dynsym, .dynstr, .hash) in an oat file are formed
 // correctly so that dynamic symbols can be successfully looked up.
 TEST_F(ElfWriterTest, CheckDynamicSection) {
@@ -231,9 +132,7 @@ TEST_F(ElfWriterTest, CheckDynamicSection) {
 
     std::string error_msg;
     std::unique_ptr<ElfFile> ef(ElfFile::Open(tmp_oat.GetFile(),
-                                              /*writable=*/ false,
-                                              /*program_header_only=*/ true,
-                                              /*low_4gb=*/ false,
+                                              /*low_4gb=*/false,
                                               &error_msg));
     ASSERT_NE(ef.get(), nullptr) << error_msg;
     ASSERT_TRUE(ef->Load(tmp_oat.GetFile(),
