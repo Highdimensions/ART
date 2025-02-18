3d29f4e2adcd4c16146b69b449032e093162bf94
Martin Stjernholm <mast@google.com>
2025-02-18 22:26:18 +0000

    Readability fixes and some API cleanup in art::hiddenapi::ApiList.
    -  Describe the layout in dex_flags_ better and be more explicit about
       the differences between the Value and DomainApi parts.
    -  Use factory functions instead of constructors with different types.
    -  Hide several functions not used outside the class.
    -  Add Value::kFuture for the de-facto Value for max-target-future.
    -  Add static assertions for more invariants.
    -  Note some strange behaviours that are intentionally not addressed,
       to keep the refactoring pure.
    
    Test: atest art_standalone_runtime_tests
    Test: m veridex
    Bug: 377676642
    Change-Id: I52c1e45da94ffa6fb87e314fb423bb9994381465

--- tools/veridex/api_list_filter.h
+++ tools/veridex/api_list_filter.h
@@ -17,9 +17,9 @@
 #ifndef ART_TOOLS_VERIDEX_API_LIST_FILTER_H_
 #define ART_TOOLS_VERIDEX_API_LIST_FILTER_H_
 
-#include <algorithm>
-#include <android-base/strings.h>
+#include <set>
 
+#include "android-base/strings.h"
 #include "base/hiddenapi_flags.h"
 
 namespace art {
@@ -46,10 +46,10 @@ class ApiListFilter {
     }
 
     if (include_invalid_list) {
-      lists_.push_back(hiddenapi::ApiList());
+      lists_.push_back(hiddenapi::ApiList::Invalid());
     }
     for (size_t i = 0; i < hiddenapi::ApiList::kValueCount; ++i) {
-      hiddenapi::ApiList list = hiddenapi::ApiList(i);
+      hiddenapi::ApiList list = hiddenapi::ApiList::FromIntValue(i);
       if (exclude_set.find(list) == exclude_set.end()) {
           lists_.push_back(list);
       }
