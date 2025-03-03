041c574fb15b228d95f281e4e478839b7af5f7b7
Mythri Alle <mythria@google.com>
2025-03-03 10:13:42 +0000

    Debug qemu 2264 failure
    Change-Id: I105a2319ae7df232bce27f0785b234f8b833c3bb

--- test/2246-trace-v2/dump_trace.cc
+++ test/2246-trace-v2/dump_trace.cc
@@ -77,6 +77,9 @@ bool ProcessThreadOrMethodInfo(std::unique_ptr<File>& file,
   if (str[str.length() - 1] == '\n') {
     str.erase(str.length() - 1);
   }
+  /*if (str.starts_with("Main")) {
+    LOG(ERROR) << "Method map " << std::hex << id << "  " << str;
+  }*/
   name_map.emplace(id, str);
   return true;
 }
@@ -189,12 +192,15 @@ bool ProcessTraceEntries(std::unique_ptr<File>& file,
     prev_method_value = curr_method_value;
     uint8_t event_type = curr_method_value & 0x3;
     uint64_t method_id = (curr_method_value >> kTraceActionBits) << kTraceActionBits;
-    if (method_map.find(method_id) == method_map.end()) {
-      LOG(FATAL) << "No entry for method " << std::hex << method_id;
+    std::string method_name;
+    if (method_map.find(method_id) != method_map.end()) {
+      method_name = method_map[method_id];
+    } else {
+      LOG(ERROR) << "No entry for method " << diff << " " << prev_method_value << " " << std::hex << method_id;
     }
     if (print_thread_events) {
       PrintTraceEntry(thread_name,
-                      method_map[method_id],
+                      method_name,
                       event_type,
                       &current_depth,
                       ignored_method,
@@ -264,14 +270,16 @@ extern "C" JNIEXPORT void JNICALL Java_Main_dumpTrace(JNIEnv* env,
         }
         break;
       case kTraceEntries:
-        ProcessTraceEntries(file,
+        if (!ProcessTraceEntries(file,
                             current_depth_map,
                             thread_map,
                             method_map,
                             is_dual_clock,
                             thread_name,
                             ignored_method_map,
-                            ignored_method_depth_map);
+                            ignored_method_depth_map)) {
+          has_entries = false;
+        }
         break;
       case kSummary:
         has_entries = false;
