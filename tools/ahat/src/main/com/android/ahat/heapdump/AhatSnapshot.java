97e0d576772116e543bc684fb3f1f975f345d09b
Thomas Bull <thomasbull@google.com>
2025-03-04 02:59:45 -0800

    Adds DuplicateString detection and analysis.
    For every instance of String in the heap dump, we check to see if another String instance with the same content already exists. If a duplicate is found, we record the size and count of each duplicated instance.
    
    Change-Id: Ibd0feb14a1250bd54fffae1cb4e6940fa8945baa
    Test: atest ahat-tests --host
    Test: manually launch ahat, view duplicate strings table and click on the strings.
    Bug: 393527371

--- tools/ahat/src/main/com/android/ahat/heapdump/AhatSnapshot.java
+++ tools/ahat/src/main/com/android/ahat/heapdump/AhatSnapshot.java
@@ -19,6 +19,7 @@ package com.android.ahat.heapdump;
 import com.android.ahat.dominators.Dominators;
 import com.android.ahat.progress.Progress;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A parsed heap dump.
@@ -39,6 +40,8 @@ public class AhatSnapshot implements Diffable<AhatSnapshot> {
 
   private AhatBitmapInstance.BitmapDumpData mBitmapDumpData = null;
 
+  private Map<String, DuplicateStrings.DuplicatedString> mDuplicatedStrings = null;
+
   AhatSnapshot(SuperRoot root,
                Instances<AhatInstance> instances,
                List<AhatHeap> heaps,
@@ -53,6 +56,8 @@ public class AhatSnapshot implements Diffable<AhatSnapshot> {
     AhatInstance.computeReachability(mSuperRoot, progress, mInstances.size());
 
     mBitmapDumpData = AhatBitmapInstance.findBitmapDumpData(mSuperRoot, mInstances);
+
+    mDuplicatedStrings = DuplicateStrings.findDuplicatedStrings(mSuperRoot, mInstances);
 
     for (AhatInstance inst : mInstances) {
       // Add this instance to its site.
@@ -220,5 +225,14 @@ public class AhatSnapshot implements Diffable<AhatSnapshot> {
    */
   public List<List<AhatBitmapInstance>> findDuplicateBitmaps() {
     return AhatBitmapInstance.findDuplicates(mBitmapDumpData);
+  }
+
+  /**
+   * Returns duplicated strings in this snapshot
+   *
+   * @return list of duplicated strings
+   */
+  public List<DuplicateStrings.DuplicatedString> findTopDuplicateStrings() {
+    return DuplicateStrings.findMostDuplicatedStrings(mDuplicatedStrings, 50);
   }
 }
