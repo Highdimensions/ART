883e893b30abe75a2f03f31772b98bc5e42eec94
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-03-04 15:17:44 -0800

    Revert "Fast field lookup in nterp."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/e6e83d1b-7939-4b2d-b7ef-ea5f4401f716). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Id189c7c275801520d94cf48f2fca957467b5af22
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- runtime/mirror/class.h
+++ runtime/mirror/class.h
@@ -1123,9 +1123,6 @@ class EXPORT MANAGED Class final : public Object {
   ArtField* FindDeclaredField(ObjPtr<DexCache> dex_cache, uint32_t dex_field_idx)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
-  ArtField* FindDeclaredField(uint32_t dex_field_idx)
-      REQUIRES_SHARED(Locks::mutator_lock_);
-
   ArtField* FindDeclaredField(std::string_view name, std::string_view type)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
