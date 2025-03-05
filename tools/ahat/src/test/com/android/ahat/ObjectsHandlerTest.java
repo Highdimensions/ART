48a6aeba6548da9096dac8dfa2e51e7be28fdd55
Thomas Bull <thomasbull@google.com>
2025-03-05 04:25:32 -0800

    Adds DuplicateString detection and analysis.
    For every instance of String in the heap dump, we check to see if another String instance with the same content already exists. If a duplicate is found, we record the size and count of each duplicated instance.
    
    Change-Id: Ibd0feb14a1250bd54fffae1cb4e6940fa8945baa
    Test: atest ahat-tests --host
    Test: manually launch ahat, view duplicate strings table and click on the strings.
    Bug: 393527371

--- tools/ahat/src/test/com/android/ahat/ObjectsHandlerTest.java
+++ tools/ahat/src/test/com/android/ahat/ObjectsHandlerTest.java
@@ -36,20 +36,38 @@ public class ObjectsHandlerTest {
 
     // We expect a single instance of DumpedStuff
     List<AhatInstance> dumped = ObjectsHandler.getObjects(
-        root, "DumpedStuff", /* subclass */ false, /* heapName */ null);
+        root, "DumpedStuff", /* subclass */ false, /* heapName */ null, /* stringHash */ null);
     assertEquals(1, dumped.size());
     assertTrue(dumped.get(0).getClassName().equals("DumpedStuff"));
 
     // We expect no direct instances of SuperDumpedStuff
     List<AhatInstance> direct = ObjectsHandler.getObjects(
-        root, "SuperDumpedStuff", /* subclass */ false, /* heapName */ null);
+        root, "SuperDumpedStuff", /* subclass */ false, /* heapName */ null, /* stringHash */ null);
     assertTrue(direct.isEmpty());
 
     // We expect one subclass instance of SuperDumpedStuff
     List<AhatInstance> subclass = ObjectsHandler.getObjects(
-        root, "SuperDumpedStuff", /* subclass */ true, /* heapName */ null);
+        root, "SuperDumpedStuff", /* subclass */ true, /* heapName */ null, /* stringHash */ null);
     assertEquals(1, subclass.size());
     assertTrue(subclass.get(0).getClassName().equals("DumpedStuff"));
     assertEquals(dumped.get(0), subclass.get(0));
+
+    // We expect there to be no instances with the hash 123.
+    List<AhatInstance> invalidHash = ObjectsHandler.getObjects(
+        root, "java.lang.String", /* subclass */ false, /* heapName */ null, /* stringHash */ "123");
+    assertEquals(0, invalidHash.size());
+
+    // We expect there to be one instance with a string value of os.version.
+    List<AhatInstance> stringHash = ObjectsHandler.getObjects(
+        root, "java.lang.String", /* subclass */ false, /* heapName */ null, /* stringHash */ "1174476494");
+    assertEquals(1, stringHash.size());
+    assertTrue(stringHash.get(0).getClassName().equals("java.lang.String"));
+    assertTrue(stringHash.get(0).asString().equals("os.version"));
+
+    // We expect stringHash to be null due to the wrong class name.
+    List<AhatInstance> wrongClass = ObjectsHandler.getObjects(
+        root, "DumpedStuff", /* subclass */ true, /* heapName */ null, /* stringHash */ "1174476494");
+    assertEquals(1, wrongClass.size());
+    assertTrue(wrongClass.get(0).getClassName().equals("DumpedStuff"));
   }
 }
