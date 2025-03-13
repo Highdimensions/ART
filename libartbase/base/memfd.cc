90e50fe3986accf0b49db1dedc92d7f2f46aa460
Elliott Hughes <enh@google.com>
2025-03-13 11:48:48 -0700

    IsSealFutureWriteSupported isn't bionic-specific.
    This was a workaround for "our ancient glibc headers are out of date", but I've modernized the relevant ones so this should just work on the host now too.
    
    Change-Id: Ie32c22a2e4707c0ba91e082b6743dd3c2d338859

--- libartbase/base/memfd.cc
+++ libartbase/base/memfd.cc
@@ -20,12 +20,10 @@
 #include <stdio.h>
 #if !defined(_WIN32)
 #include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/syscall.h>
 #include <sys/utsname.h>
 #include <unistd.h>
-#endif
-#if defined(__BIONIC__)
-#include <linux/memfd.h>  // To access memfd flags.
 #endif
 
 #include <android-base/logging.h>
@@ -55,17 +53,6 @@ int memfd_create(const char* name, unsigned int flags) {
   return syscall(__NR_memfd_create, name, flags);
 }
 
-#else  // __NR_memfd_create
-
-int memfd_create([[maybe_unused]] const char* name, [[maybe_unused]] unsigned int flags) {
-  errno = ENOSYS;
-  return -1;
-}
-
-#endif  // __NR_memfd_create
-
-#if defined(__BIONIC__)
-
 static bool IsSealFutureWriteSupportedInternal() {
   android::base::unique_fd fd(art::memfd_create("test_android_memfd", MFD_ALLOW_SEALING));
   if (fd == -1) {
@@ -87,12 +74,17 @@ bool IsSealFutureWriteSupported() {
   return is_seal_future_write_supported;
 }
 
-#else
+#else  // __NR_memfd_create
+
+int memfd_create([[maybe_unused]] const char* name, [[maybe_unused]] unsigned int flags) {
+  errno = ENOSYS;
+  return -1;
+}
 
 bool IsSealFutureWriteSupported() {
   return false;
 }
 
-#endif
+#endif  // __NR_memfd_create
 
 }  // namespace art
