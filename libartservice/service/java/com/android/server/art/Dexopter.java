5aae278de417542f165bd04daf02383fdd1fc78c
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-04-14 11:27:44 -0700

    Revert "Delete SDM and SDC files on a successful dexopt."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/58dfef0a-cdce-4389-8e40-e95fb8d8cbdc). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Iae88e5b3b6592c359b81a8af82aa42bd3fefee2d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- libartservice/service/java/com/android/server/art/Dexopter.java
+++ libartservice/service/java/com/android/server/art/Dexopter.java
@@ -201,15 +201,14 @@ public abstract class Dexopter<DexInfoType extends DetailedDexInfo> {
                     long sizeBeforeBytes = 0;
                     Dex2OatResult dex2OatResult = Dex2OatResult.notRun();
                     @DexoptResult.DexoptResultExtendedStatusFlags int extendedStatusFlags = 0;
-                    DexoptTarget<DexInfoType> target = null;
                     try {
-                        target = DexoptTarget.<DexInfoType>builder()
-                                         .setDexInfo(dexInfo)
-                                         .setIsa(abi.isa())
-                                         .setIsInDalvikCache(isInDalvikCache)
-                                         .setCompilerFilter(compilerFilter)
-                                         .setDmPath(dmInfo.dmPath())
-                                         .build();
+                        var target = DexoptTarget.<DexInfoType>builder()
+                                             .setDexInfo(dexInfo)
+                                             .setIsa(abi.isa())
+                                             .setIsInDalvikCache(isInDalvikCache)
+                                             .setCompilerFilter(compilerFilter)
+                                             .setDmPath(dmInfo.dmPath())
+                                             .build();
                         var options = GetDexoptNeededOptions.builder()
                                               .setProfileMerged(profileMerged)
                                               .setFlags(mParams.getFlags())
@@ -319,9 +318,6 @@ public abstract class Dexopter<DexInfoType extends DetailedDexInfo> {
                         AsLog.i(String.format("Dexopt result: [packageName = %s] %s",
                                 mPkgState.getPackageName(), result));
                         results.add(result);
-
-                        onDexoptTargetResult(target, status);
-
                         if (status != DexoptResult.DEXOPT_SKIPPED
                                 && status != DexoptResult.DEXOPT_PERFORMED) {
                             succeeded = false;
@@ -748,12 +744,6 @@ public abstract class Dexopter<DexInfoType extends DetailedDexInfo> {
      * the noop compiler filter.
      */
     protected void onDexoptStart(@NonNull DexInfoType dexInfo) throws RemoteException {}
-
-    /**
-     * Called once for every dex file and every ABI when dexopt has a result.
-     */
-    protected void onDexoptTargetResult(@NonNull DexoptTarget<DexInfoType> target,
-            @DexoptResult.DexoptResultStatus int status) throws RemoteException {}
 
     @AutoValue
     abstract static class DexoptTarget<DexInfoType extends DetailedDexInfo> {
