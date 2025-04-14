10aa6023e61801e49238815aaa867d6b55e98fc5
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-04-14 11:27:44 -0700

    Revert "Delete SDM files on "pm delete-dexopt" and "pm compile -..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/58dfef0a-cdce-4389-8e40-e95fb8d8cbdc). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Id1a97ca404f8f65e8d1692e80f775c6f26bc48e9
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- libartservice/service/java/com/android/server/art/ArtFileManager.java
+++ libartservice/service/java/com/android/server/art/ArtFileManager.java
@@ -101,7 +101,6 @@ public class ArtFileManager {
     public WritableArtifactLists getWritableArtifacts(@NonNull PackageState pkgState,
             @NonNull AndroidPackage pkg, @NonNull Options options) throws RemoteException {
         List<ArtifactsPath> artifacts = new ArrayList<>();
-        List<SecureDexMetadataWithCompanionPaths> sdmFiles = new ArrayList<>();
         List<RuntimeArtifactsPath> runtimeArtifacts = new ArrayList<>();
 
         if (options.forPrimaryDex()) {
@@ -109,9 +108,6 @@ public class ArtFileManager {
             for (PrimaryDexInfo dexInfo : PrimaryDexUtils.getDexInfo(pkg)) {
                 for (Abi abi : Utils.getAllAbis(pkgState)) {
                     artifacts.add(AidlUtils.buildArtifactsPathAsInput(
-                            dexInfo.dexPath(), abi.isa(), isInDalvikCache));
-                    // SDM files are only for primary dex files.
-                    sdmFiles.add(AidlUtils.buildSecureDexMetadataWithCompanionPaths(
                             dexInfo.dexPath(), abi.isa(), isInDalvikCache));
                     // Runtime images are only generated for primary dex files.
                     runtimeArtifacts.add(AidlUtils.buildRuntimeArtifactsPath(
@@ -129,7 +125,7 @@ public class ArtFileManager {
             }
         }
 
-        return new WritableArtifactLists(artifacts, sdmFiles, runtimeArtifacts);
+        return new WritableArtifactLists(artifacts, runtimeArtifacts);
     }
 
     /** Returns artifacts that are usable, regardless of whether they are writable. */
@@ -247,7 +243,6 @@ public class ArtFileManager {
     }
 
     public record WritableArtifactLists(@NonNull List<ArtifactsPath> artifacts,
-            @NonNull List<SecureDexMetadataWithCompanionPaths> sdmFiles,
             @NonNull List<RuntimeArtifactsPath> runtimeArtifacts) {}
 
     public record UsableArtifactLists(@NonNull List<ArtifactsPath> artifacts,
--- libartservice/service/java/com/android/server/art/ArtManagerLocal.java
+++ libartservice/service/java/com/android/server/art/ArtManagerLocal.java
@@ -202,8 +202,8 @@ public final class ArtManagerLocal {
     }
 
     /**
-     * Deletes dexopt artifacts (including cloud dexopt artifacts) of a package, for primary dex
-     * files and for secondary dex files. This includes VDEX, ODEX, ART, SDM, and SDC files.
+     * Deletes dexopt artifacts of a package, including the artifacts for primary dex files and the
+     * ones for secondary dex files. This includes VDEX, ODEX, and ART files.
      *
      * Also deletes runtime artifacts of the package, though they are not dexopt artifacts.
      *
@@ -231,9 +231,6 @@ public final class ArtManagerLocal {
             }
             for (RuntimeArtifactsPath runtimeArtifacts : list.runtimeArtifacts()) {
                 freedBytes += mInjector.getArtd().deleteRuntimeArtifacts(runtimeArtifacts);
-            }
-            for (SecureDexMetadataWithCompanionPaths sdmSdcFiles : list.sdmFiles()) {
-                freedBytes += mInjector.getArtd().deleteSdmSdcFiles(sdmSdcFiles);
             }
             return DeleteResult.create(freedBytes);
         } catch (RemoteException e) {
@@ -393,17 +390,13 @@ public final class ArtManagerLocal {
     }
 
     /**
-     * Resets the dexopt state of the package as if the package is newly installed without cloud
-     * dexopt artifacts (SDM files).
+     * Resets the dexopt state of the package as if the package is newly installed.
      *
-     * More specifically,
-     * - It clears current profiles, reference profiles, and all dexopt artifacts (including cloud
-     *   dexopt artifacts).
-     * - If there is an external profile (e.g., a cloud profile), the reference profile will be
-     *   re-created from that profile, and dexopt artifacts will be regenerated for that profile.
+     * More specifically, it clears reference profiles, current profiles, any code compiled from
+     * those local profiles, and runtime artifacts. If there is an external profile (e.g., a cloud
+     * profile), the code compiled from that profile will be kept.
      *
-     * For secondary dex files, it clears all profiles and dexopt artifacts without regeneration
-     * because secondary dex files are supposed to be unknown at install time.
+     * For secondary dex files, it also clears all dexopt artifacts.
      *
      * @hide
      */
